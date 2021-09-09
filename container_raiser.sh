fd972e0aa6dcf6a5da6106889af8db3d0ee87a8e
Seiya Sasaki <sirukufarios@gmail.com>
2021-09-09 17:14:34 +0900

    Improvement: enable to switch mode with argument for shell

--- container_raiser.sh
+++ container_raiser.sh
@@ -1,3 +1,8 @@
 # This gives us the IP address of host OS.
 export HOST_IP_ADDRESS=`ifconfig eth0 | grep inet | grep -v inet6 | sed -E "s/inet ([0-9]{1,3}.[0-9]{1,3}.[0-9].{1,3}.[0-9]{1,3}) .*$/\1/" | tr -d "\t"`
-docker-compose up -d
+
+if [ "$1" = 'production' ]; then
+    docker-compose up -d
+else
+    docker-compose up
+fi
